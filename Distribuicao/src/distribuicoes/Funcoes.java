e3d34fe71732c36677b24f407b1fde1c5575cb0a
Tobias Silva <tobiassilva@hotmail.it>
2019-09-30 17:39:20 -0300

    correções na formula da distribuição normal

--- Distribuicao/src/distribuicoes/Funcoes.java
+++ Distribuicao/src/distribuicoes/Funcoes.java
@@ -16,15 +16,17 @@ public class Funcoes {
 		System.out.println("Media Aritmetica: " +mi);
 		System.out.println("Desvio padrao: " +dp);
 		
-		double v1 = 1/(dp*Math.sqrt(2*Math.PI)); // Primeira parte da formula, 1 sobre desvio padrao X 2 x Pi
-		
+		double valor1 = 1/(dp*Math.sqrt(2*Math.PI)); // Primeira parte da formula, 1 sobre desvio padrao X 2 x Pi
+		double v3;
 		double []fx = new double [a.length];
 		double valor2;
 		
 		for (int x=0; x <= a.length-1; x++) {
-			valor2 = (a[x] - mi)/(2*(dp*dp)); //Segunda parte da formula, x menos a media sobre 2 X a variancia
-			valor2 = valor2 * -1;
-			fx[x] = v1 * Math.pow(Math.E,valor2 ); // Multiplicando as partes valor 1 x Euler a menos valor 2
+			v3 = Math.pow((aux[x] - mi),2);
+			//System.out.println(v3);
+			valor2 = v3/(2*(dp*dp)); //Segunda parte da formula, x menos a media sobre 2 X a variancia
+			valor2 = valor2 * (-1);
+			fx[x] = valor1 * Math.pow(Math.E,valor2 ); // Multiplicando as partes valor 1 x Euler a menos valor 2
 			
 			System.out.println("x ="+ aux[x]+ " -> f(x)= " + fx[x]);
 		}
